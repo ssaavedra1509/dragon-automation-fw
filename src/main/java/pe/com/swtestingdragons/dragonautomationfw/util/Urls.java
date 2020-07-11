fb4925ccd803ffe71f82ef84e2b8bbb8d22da9ef
Jose Hurtado <josehumberto17@gmail.com>
2020-07-11 13:20:41 -0500

    Update Urls.java

--- src/main/java/pe/com/swtestingdragons/dragonautomationfw/util/Urls.java
+++ src/main/java/pe/com/swtestingdragons/dragonautomationfw/util/Urls.java
@@ -2,6 +2,6 @@ package pe.com.swtestingdragons.dragonautomationfw.util;
 
 public class Urls {
     //public static final String REDMINE_LOGIN = "http://localhost:8081/login";
-    public static final String REDMINE_LOGIN = "http://b74e2b031ed1.ngrok.io/login";
-    public static final String REDMINE_URL = "http://b74e2b031ed1.ngrok.io";
+    public static final String REDMINE_LOGIN = "http://3994530a1317.ngrok.io/login";
+    public static final String REDMINE_URL = "http://3994530a1317.ngrok.io";
 }
