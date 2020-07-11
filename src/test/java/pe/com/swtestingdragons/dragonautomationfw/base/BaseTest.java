65f5d0c1b59e3b7893e66cebddaa123dbfb721d7
Jose Hurtado <josehumberto17@gmail.com>
2020-07-11 05:12:49 -0500

    Add new chrome drivers for diferents platforms

--- src/test/java/pe/com/swtestingdragons/dragonautomationfw/base/BaseTest.java
+++ src/test/java/pe/com/swtestingdragons/dragonautomationfw/base/BaseTest.java
@@ -7,38 +7,42 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import pe.com.swtestingdragons.dragonautomationfw.pages.RedmineLoginPage;
 import pe.com.swtestingdragons.dragonautomationfw.util.Urls;
 
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
 
     private static WebDriver driver;
     protected static RedmineLoginPage redmineLoginPage;
 
     @BeforeClass
-    public static void setUpAndLaunchApp(){
+    public static void setUpAndLaunchApp() {
 
-       setChromeDriverProperty();
+        setChromeDriverProperty();
 
-       driver = new ChromeDriver();
-       //Implicit wait
-       //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-       driver.get(Urls.REDMINE_LOGIN);
-       redmineLoginPage = new RedmineLoginPage(driver);
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get(Urls.REDMINE_LOGIN);
+
+        redmineLoginPage = new RedmineLoginPage(driver);
     }
 
     @AfterClass
-    public static void tearDown(){
+    public static void tearDown() {
         driver.quit();
     }
 
-    private static void setChromeDriverProperty(){
-        if(System.getProperty("os.name").contains("Windows")) {
-            System.setProperty("webdriver.chrome.driver", "resources/drivers/chromedriver.exe");
-        }
-        else{
-            System.setProperty("webdriver.chrome.driver", "resources/drivers/chromedriver");
+    private static void setChromeDriverProperty() {
+        if (System.getProperty("os.name").contains("Windows")) {
+            System.setProperty("webdriver.chrome.driver", "resources/drivers/windows/chromedriver83.exe");
+        } else if (System.getProperty("os.name").contains("Linux")) {
+            System.setProperty("webdriver.chrome.driver", "resources/drivers/linux/chromedriver83");
+        } else if (System.getProperty("os.name").contains("Mac")) {
+            System.setProperty("webdriver.chrome.driver", "resources/drivers/mac/chromedriver83");
         }
     }
 
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
         return driver;
     }
 }
