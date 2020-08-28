bff3a1c95a43525b41ba788ff4a0f30d091af106
Jose Hurtado <josehumberto17@gmail.com>
2020-08-27 21:32:49 -0500

    Actualización de url

--- src/main/java/pe/com/swtestingdragons/dragonautomationfw/util/Urls.java
+++ src/main/java/pe/com/swtestingdragons/dragonautomationfw/util/Urls.java
@@ -6,5 +6,5 @@ public class Urls {
     public static final String REDMINE_LOGIN = "http://b74e2b031ed1.ngrok.io/login";
     public static final String REDMINE_URL = "http://b74e2b031ed1.ngrok.io";
     public static final String EXCHANGE_RATE_FRONTEND_URL = "http://localhost:4200";
-    public static final String EXCHANGE_RATE_BACKEND_URL = "http://088d8716ada7.ngrok.io";
+    public static final String EXCHANGE_RATE_BACKEND_URL = "http://8c98cfe595a8.ngrok.io";
 }
