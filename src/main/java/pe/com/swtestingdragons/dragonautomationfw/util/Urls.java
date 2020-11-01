6048ae34802aeccadfec0435e9afdc6223b8ce12
ssaavedra1509 <52169077+ssaavedra1509@users.noreply.github.com>
2020-11-01 13:29:17 -0500

    Update Urls.java

--- src/main/java/pe/com/swtestingdragons/dragonautomationfw/util/Urls.java
+++ src/main/java/pe/com/swtestingdragons/dragonautomationfw/util/Urls.java
@@ -3,8 +3,8 @@ package pe.com.swtestingdragons.dragonautomationfw.util;
 public class Urls {
     //public static final String REDMINE_LOGIN = "http://localhost:8081/login";
 
-    public static final String REDMINE_LOGIN = "http://b74e2b031ed1.ngrok.io/login";
-    public static final String REDMINE_URL = "http://b74e2b031ed1.ngrok.io";
+    public static final String REDMINE_LOGIN = "http://023713eca62a.ngrok.io/login";
+    public static final String REDMINE_URL = "http://023713eca62a.ngrok.io/";
     public static final String EXCHANGE_RATE_FRONTEND_URL = "http://localhost:4200";
     public static final String EXCHANGE_RATE_BACKEND_URL = "http://8c98cfe595a8.ngrok.io";
 }
